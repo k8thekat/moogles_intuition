8827100eee69db71b0afcebd38b9d9361705e841
k8thekat <cadwalladerkatelynn@gmail.com>
2025-07-28 13:35:21 -0700

    # Dependency update. - Added `async_universalis`.

--- moogle_intuition/__init__.py
+++ moogle_intuition/__init__.py
@@ -23,7 +23,7 @@ from __future__ import annotations
 __title__ = "Moogle's Intuition"
 __author__ = "k8thekat"
 __license__ = "GNU"
-__version__ = "0.1.0-dev"
+__version__ = "0.1.1-dev"
 __credits__ = "Universalis, GarlandTools and SquareEnix"
 
 from typing import Literal, NamedTuple
@@ -40,6 +40,6 @@ class VersionInfo(NamedTuple):  # noqa: D101
     release_level: Literal["release", "development"]
 
 
-version_info: VersionInfo = VersionInfo(major=0, minor=1, revision=0, release_level="development")
+version_info: VersionInfo = VersionInfo(major=0, minor=1, revision=1, release_level="development")
 
 del version_info, VersionInfo
